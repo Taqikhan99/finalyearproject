e35708a8c029a2069c324a612a00053df9c3838f
TaqiKhan <68019464+Taqikhan99@users.noreply.github.com>
2021-12-18 10:25:32 +0500

    started using face-recognition library

---
+++ dbconnection.py
@@ -0,0 +1,14 @@
+import pyodbc
+
+# database connection class
+class DbConnection:
+    def __init__(self) -> None:
+        self.cursor=None
+    def getCursor(self):
+        return self.cursor
+    def connectToDb(self):
+
+        # creating connection
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=TAQILAPTOP\SQLEXPRESS2019;DATABASE=TaqiComputers_DB;Trusted_Connection=yes;')
+        self.cursor=connection.cursor()
+        print("Connection successful")
